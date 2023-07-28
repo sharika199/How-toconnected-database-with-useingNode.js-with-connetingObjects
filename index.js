4530697816c1836fa959d07de7554bc30471df2d
sharika199 <chitturisharika@gmail.com>
2023-07-28 15:05:21 +0000

    How-toconnected-database-with-useingNode.js-with-connetingObjects.git

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+let db = null;
+let dbPath = path.join(__dirname, "goodreads.db");
+const ini = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("sever Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error:${e.message}`);
+    process.exit(1);
+  }
+};
+ini();
